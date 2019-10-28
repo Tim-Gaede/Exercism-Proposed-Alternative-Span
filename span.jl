144dd52a890d0bfc145a45f0485cff97079fb6ea
Tim-Gaede <TimGaede@gmail.com>
2019-10-28 08:19:55 -0400

    Update span.jl

--- span.jl
+++ span.jl
@@ -1,11 +1,4 @@
 function span(a::Array{Any,1})
-    lo = hi = first(a)
-    for i = 2 : length(a)
-        if     a[i] < lo;    lo = a[i]
-        elseif a[i] > hi;    hi = a[i]
-        end
-    end
-
-    lo, hi # returned
+    
 end
  
